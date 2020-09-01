563ced61c38271c55fbceb50cd8118acdfce020c
Stan Naspinski <stann@emergentsoftware.net>
2020-09-01 15:04:57 -0500

    demo stuff

---
+++ UmbracoFormsDemo/Controllers/ServicesController.cs
@@ -0,0 +1,34 @@
+﻿using ImageProcessor.Imaging.Colors;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoFormsDemo.Controllers
+{
+    public class ServicesController : SurfaceController
+    {
+        [HttpGet]
+        [ActionName("pre")]
+        public XmlResult PreValues()
+        {
+            var strings = new List<string>()
+            {
+                "values",
+                "returned",
+                "from",
+                "ServicesController.PreValues()"
+            };
+            return new XmlResult(strings);
+        }
+
+        [HttpPost]
+        [ActionName("handle-post")]
+        public ActionResult HandlePost()
+        {
+            var form = Request.Form.AllKeys.ToDictionary(x => x, y => Request.Form[y]);
+            return new HttpStatusCodeResult(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
---
+++ UmbracoFormsDemo/Controllers/XmlResult.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace UmbracoFormsDemo.Controllers
+{
+    // https://stackoverflow.com/questions/134905/return-xml-from-a-controllers-action-in-as-an-actionresult
+    public class XmlResult : ActionResult
+    {
+        private object objectToSerialize;
+        public XmlResult(object objectToSerialize)
+        {
+            this.objectToSerialize = objectToSerialize;
+        }
+        public object ObjectToSerialize
+        {
+            get { return this.objectToSerialize; }
+        }
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (this.objectToSerialize != null)
+            {
+                context.HttpContext.Response.Clear();
+                var xs = new System.Xml.Serialization.XmlSerializer(this.objectToSerialize.GetType());
+                context.HttpContext.Response.ContentType = "text/xml";
+                xs.Serialize(context.HttpContext.Response.Output, this.objectToSerialize);
+            }
+        }
+    }
+}
